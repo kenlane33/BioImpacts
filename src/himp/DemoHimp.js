c5035c0974fb50f774d223dbaa3e7e944fe96df7
Ken Lane <kenlane33@gmail.com>
2021-03-18 16:56:10 -0400

    himp.js w passed in Comps

--- src/himp/DemoHimp.js
+++ src/himp/DemoHimp.js
@@ -1,11 +1,12 @@
 
 import {Marky} from '../components/marky'
 import  React  from 'react'
-import {prepStruc, parseImp} from '../himp/himp'
+import {prepStruc} from '../himp/himp'
 import {exampleStrucs} from '../himp/exampleStrucs'
-import {safeIth, trimAll,noBlanks,splitTrim, makeDoFnOnEachFn} from '../helpers/array'
+import {safeIth} from '../helpers/array'
+import {runImps} from './himp'
 
-//======================/////============================
+//======================/////////============================
 export default function DemoHimp() {
 
   const jours = {
@@ -16,43 +17,12 @@ export default function DemoHimp() {
   }
 
   const structures = prepStruc(exampleStrucs, jours)
+  console.log( 'structures=', structures )
 
   const ff = [
     {txt:'\n## Fooness\n**foo** boo', tag:'#21.R.Mild'},
     {txt:null, tag:'#SpecialTag'},
   ]
-  
-  console.log( 'structures=', structures )
-
-  const matchAncestorPick = (struc, val, flavor) => {
-    return (flavor) ? 
-      struc.ancFlavPicks.includes(`${flavor||''}_${val}`) :
-      struc.ancPicks.includes(val)
-    // let wasFound = false
-    // struc.ancestors().some( (x) => { // similar to forEach, but returning true breaks the loop
-    //   wasFound ||= ((safeIth( x.pick, 1 )+'') === val) // console.log(`if(${valToMatch}) p1==${p1}?${(p1 === valToMatch)}, p2==${p2}?${(p2 === valToMatch)}, val=${valToMatch}, Pick=${x.pick.slice(0,-1)}`)
-    //   wasFound ||= ((safeIth( x.pick, 2 )+'') === val) // checks index as well?
-    //   if (flavor) {
-    //     wasFound &&= (x.flavor[0] === flavor[0]) // console.log(x.pick[1], x.flavor[0], flavorToMatch[0], x.flavor[0] === flavorToMatch[0])
-    //   }
-    //   return wasFound // if false keep looping b/c of the way .some() works, if true, done, so break!
-    // })
-    // return wasFound // this returns the final result from the func
-  }
-
-  const calcIf = (ifParams, struc, verb) => {
-    // If any of the params match the picks of self or ancestors with the right flavor
-    const flavorToMatch = safeIth(verb,2) // grabs the X in ifX()
-    let gotMatch = false
-    if (ifParams) {
-      const ps = splitTrim(ifParams, ',')
-      ps.some( (p) => { // some() is akin to forEach(), but returning true breaks the loop
-        gotMatch ||= matchAncestorPick(struc, p, flavorToMatch) 
-        return gotMatch // this will break the loop as soon as a match is found since we called some()
-      })
-    } 
-    return gotMatch
-  }
 
   const tfStr = (tf) => (tf) ? "T" : "F"
   const SimpleImp =({tf,parts,stl={},elType='d'}) => {
@@ -85,36 +55,8 @@ export default function DemoHimp() {
     )
     return(<SimpleImp tf={tf} parts={`say('${txt}')`} elType='s' />)
   }
-  const runImp = (impRaw, struc, Comps) => {
-    const imp = parseImp(impRaw) 
-    let ifExpResult = false
-    return noBlanks(imp).map((impParts) => {
-      const [verb, params] = trimAll(impParts)
-      //console.log( '[verb, params]', [verb, params] )
-      if (      verb.startsWith("if") ) {
-        ifExpResult = calcIf(params, struc, verb)
-        return <SimpleImp tf={ifExpResult} parts={impParts} />
-      }
-      else if ( verb.startsWith("andIf") ) {
-        ifExpResult = ifExpResult && calcIf(params, struc, verb.slice(3)) // slice to chop off the 'and' from 'andIf()
-        return <SimpleImp tf={ifExpResult} parts={impParts} />
-      } 
-      else if (verb === "say") {
-        return <Comps.Say tf={ifExpResult} parts={impParts} />
-      } 
-      else if (verb === "sumSay") {
-        return <Comps.SumSay tf={ifExpResult} parts={impParts} />
-      } 
-      else if (verb === "fix") {
-        return <FixImp tf={ifExpResult} parts={impParts} />
-      } 
-      else {
-        return <SimpleImp tf={ifExpResult} parts={impParts} />
-      }
-    })
-  }
 
-  const runImps = makeDoFnOnEachFn(runImp)
+
   var keySafe = {}
   const ks = (k) => {
     if(keySafe[k]) {
@@ -130,10 +72,9 @@ export default function DemoHimp() {
     if(!struc.id) return null
     return [
       <div key={ks('top_'+(struc.id || "?"))} style={{marginTop:20, paddingTop:4,borderTop:'1px solid grey'}}>
-        {`${struc.flavor} : ${struc.name} [${struc.id}]`}
-
-        <br />
-
+        <div key={ks('struc_'+struc.id)}>
+          {`${struc.flavor} : ${struc.name} [${struc.id}]`}
+        </div>
         <div key={ks("picker_"+struc.id)} style={{marginLeft: 30, color: "#066"}}>
           {struc.picker}
         </div>
@@ -145,13 +86,6 @@ export default function DemoHimp() {
         <div key={ks('imps_'+struc.id)} style={{marginLeft: 5, color: "#606"}}>
           {runImps(struc.impacts, struc, Comps)}
         </div>
-        {/* <div  key={'json'+struc.id} style={{marginLeft: 40, color: "#600"}}>
-          <pre>{JSON.stringify(struc.impacts, null, 2)}</pre>
-          <pre>{JSON.stringify(struc.parentImpacts, null, 2)}</pre>
-        </div> */}
-        {/* <pre style={{fontSize: 10}}>
-          {JSON.stringify({...struc, children: undefined}, null, 2)}
-        </pre> */}
       </div>,
       Strucs({strucs: struc.children, Comps}) // recurse
     ]
@@ -164,12 +98,23 @@ export default function DemoHimp() {
   )
   const Foxer = ({txt}) => <div>Fox: {txt}</div>
   const CatImg = ()=><img alt="" style={{width:50, display:'inline', verticalAlign: 'middle'}} src="https://octodex.github.com/images/stormtroopocat.jpg"/>
-
+  const StrikeImp = ({tf,parts}) =>(
+    <pre>{`${tfStr(tf)}: ${parts.join('(')})`}</pre>
+  )
   //-------------------------------------------
+  const comps = {
+    if:SimpleImp,
+    andIf:SimpleImp,
+    say:SayImp, 
+    sumSay:SayImp, 
+    fix:FixImp,
+    strikethrough: StrikeImp,
+    Raw:SimpleImp,
+  }
   return (
     <div className="App">
       <h3>Health Impact Code (Himp)</h3>
-      <Strucs strucs={structures} Comps={{Say:SayImp, SumSay:SayImp, Fix:FixImp, Raw:SimpleImp}}/>
+      <Strucs strucs={structures} Comps={comps}/>
       <h1>☯</h1>
     </div>
   )
--- src/himp/himp.js
+++ src/himp/himp.js
@@ -1,4 +1,4 @@
-import {noBlanks} from '../helpers/array'
+import {safeIth, trimAll,noBlanks,splitTrim, makeDoFnOnEachFn} from '../helpers/array'
 
 const parseImp = (imp) => (
   imp.replace(/\)[\s\n]*if/gm,').\nif').split(/\s*\./g).map((x) => {
@@ -38,4 +38,56 @@ const prepStrucs = (strucs, jours) =>(
   (strucs && strucs.map((x) => prepStruc(x, jours) )) || []
 )
 
-export {prepStruc, parseImp}
+const matchAncestorPick = (struc, val, flavor) => {
+  return (flavor) ? 
+    struc.ancFlavPicks.includes(`${flavor||''}_${val}`) :
+    struc.ancPicks.includes(val)
+}
+
+const calcIf = (ifParams, struc, verb) => {
+  // If any of the params match the picks of self or ancestors with the right flavor
+  const flavorToMatch = safeIth(verb,2) // grabs the X in ifX()
+  let gotMatch = false
+  if (ifParams) {
+    const ps = splitTrim(ifParams, ',')
+    ps.some( (p) => { // some() is akin to forEach(), but returning true breaks the loop
+      gotMatch ||= matchAncestorPick(struc, p, flavorToMatch) 
+      return gotMatch // this will break the loop as soon as a match is found since we called some()
+    })
+  } 
+  return gotMatch
+}
+
+const runImp = (impRaw, struc, Comps) => {
+  const imp = parseImp(impRaw) 
+  let ifResult = false
+  return noBlanks(imp).map((impParts) => {
+    const [verb, params] = trimAll(impParts)
+    let CompForVerb = Comps[verb]
+    //console.log( '[verb, params]', [verb, params] )
+    //-------------------------///--------------------------
+    if (      verb.startsWith("if") ) {
+      ifResult = calcIf(params, struc, verb)
+      const Comp = Comps.if
+      return <Comp tf={ifResult} parts={impParts} />
+    }
+    //-------------------------//////--------------------------
+    else if ( verb.startsWith("andIf") ) {
+      ifResult = ifResult && calcIf(params, struc, verb.slice(3)) // slice to chop off the 'and' from 'andIf()
+      const Comp = Comps.if
+      return <Comp tf={ifResult} parts={impParts} />
+    } 
+    //----------//////--------------------------
+    else if (CompForVerb) {
+      return <CompForVerb tf={ifResult} parts={impParts} />
+    } 
+    else {
+      return <Comps.Raw tf={ifResult} parts={impParts} />
+    }
+  })
+}
+
+const runImps = makeDoFnOnEachFn(runImp)
+
+
+export {prepStruc, parseImp, runImp, runImps}
