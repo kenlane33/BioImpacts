fdea6de16b23ad067ee6442514a9b31f23afb14e
Ken Lane <kenlane33@gmail.com>
2021-03-22 14:56:13 -0400

    Prettier example Strucs

--- src/himp/exampleStrucs.js
+++ src/himp/exampleStrucs.js
@@ -16,20 +16,31 @@ const exampleStrucs =
           id: 3,
           name: "Sleep apena",
           impacts:[`
-ifC(Mild,OR,Moderate).set(life,-3).tag(#lungy).say(Strike ME!).say(
+ifC(Mild,OR,Moderate).tag(#lungy)
+  .set(life,-3)
+  .say(Strike ME!)
+  .say(
 **Sleep apnea:** *periodically* **stop breathing** when asleep
-).pointer( lung-R, Difficulty breathing, red ).hurt(#Sum.life,-4%)
-.sumRank(40).sumSay(  
-**Summary - Mild stuff**
-)
-ifC(Mild).andIf(1).say(    
+  )
+  .pointer( lung-R, Difficulty breathing, red )
+  .hurt(#Sum.life,-4%)
+  .sumRank(40)
+  .sumSay(  
+**Summary - Mild or Moderate stuff**
+  )
+
+ifC(Mild).andIfC(1)
+.say(  
 Severe **stuff** *in* **markdown** that would hide if parent \`ifC\` is not Severe
-)
+  )
           `,
-          'if(Boom).say(Woo)',
-          `if(Mild).say(Feel tired).say(Dry mouth).say(Mild snoring)`,
-          `if(Mild,OR,Moderate).prior().say(Diabetes).say(Strokes).say(Heart attacks)`,
-          `if(Severe).prior().say(Sudden death while sleeping)`,
+          `if(Boom)
+             .say(Woo)`,
+          `if(Mild)
+             .say(Feel tired).say(Dry mouth).say(Mild snoring)`,
+          `if(Mild,OR,Moderate)
+             .say(Diabetes).say(Strokes).say(Heart attacks)`,
+          `if(Severe).say(Sudden death while sleeping)`,
           ],
           children: [
             {
@@ -40,14 +51,21 @@ Severe **stuff** *in* **markdown** that would hide if parent \`ifC\` is not Seve
               impacts: [
                 `ifAct(Nightly).strikeThrough(#lungy)`,
                 "if(Never    ).doNothing()",
-                "ifRisk(Moderate).andIfAction(Nightly  ).strikeSays(Moderate).say(Reduces moderate & severe impacts.)",
-                `ifAction(Nightly  ).delete(Moderate).fix(#Sum.life,+4%).strikeSays(Severe).say(
+                `ifRisk(Moderate).andIfAction(Nightly  )
+                   .strikeSays(Moderate)
+                   .say(Reduces moderate & severe impacts.)`,
+                `ifAction(Nightly  )
+                   .delete(Moderate)
+                   .fix(#Sum.life,+4%)
+                   .strikeSays(Severe)
+                   .say(
 **Nightly** is the best choice<br/>
 <span>**Removes** moderate & **severe** *impacts*</span>
 
 <CatImg /> <span> <-- a React component with an image </span>
 <Foxer txt=": this is a silly React component that adds the word Fox as a prefix"/>
-                  ).delete()
+                  )
+                  .delete()
                 `
               ]
             }
